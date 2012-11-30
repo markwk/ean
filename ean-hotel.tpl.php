f83b3388891544bad143d175bf7d37b21e466e25
Noë Snaterse <info@wiredpea.com>
2012-11-30 14:13:26 +0100

    Finished the test menu.

--- ean-hotel.tpl.php
+++ ean-hotel.tpl.php
@@ -1,51 +1,53 @@
 <div class="ean-hotel">
   <div class="ean-hotel-summary">
-    <?php print $HotelInformationResponse['HotelSummary']['name'] ?>,
-    <?php print $HotelInformationResponse['HotelSummary']['hotelRating'] ?>
+    <?php print $hotel['HotelSummary']['name'] ?>,
+    <?php print $hotel['HotelSummary']['hotelRating'] ?>
     <?php
-    if($HotelInformationResponse['HotelSummary']['hotelRating'] == 1) {
+    if($hotel['HotelSummary']['hotelRating'] == 1) {
       t('Star');
     } else {
       t('Stars');
     }
     ?>
     <br />
-    <?php print $HotelInformationResponse['HotelSummary']['address1'] ?><br />
-    <?php print $HotelInformationResponse['HotelSummary']['city'] ?>,
-    <?php print $HotelInformationResponse['HotelSummary']['postalCode'] ?>,
-    <?php print $HotelInformationResponse['HotelSummary']['countryCode'] ?><br />
-    <?php print $HotelInformationResponse['HotelSummary']['locationDescription'] ?><br />
-    (<?php print $hotelInformationResponse['HotelSummary']['latitude'] ?>,
-    <?php print $hotelInformationResponse['HotelSummary']['longitude'] ?>)
+    <?php print $hotel['HotelSummary']['address1'] ?><br />
+    <?php print $hotel['HotelSummary']['city'] ?>,
+    <?php print $hotel['HotelSummary']['postalCode'] ?>,
+    <?php print $hotel['HotelSummary']['countryCode'] ?><br />
+    <?php print $hotel['HotelSummary']['locationDescription'] ?><br />
+    (<?php print $hotel['HotelSummary']['latitude'] ?>,
+    <?php print $hotel['HotelSummary']['longitude'] ?>)
   </div>
 
   <div class="ean-hotel-amenities">
-    <? $amenities = array();
-    <? foreach $hotelInformationResponse['PropertyAmenities']['PropertyAmenity'] as $amenity ?>
+    <? $amenities = array(); ?>
+    <? foreach ($hotel['PropertyAmenities']['PropertyAmenity'] as $amenity): ?>
       <? $amenities[] = $amenity['amenity'] ?>
     <? endforeach ?>
     <? print implode($amenities, ', ') ?>
   </div>
 
   <div class="ean-hotel-details">
-    Number of rooms: <? print $hotelInformationResponse['HotelDetails']['numberOfRooms'] ?><br />
-    Number of floors: <? print $hotelInformationResponse['HotelDetails']['numberOfFloors'] ?><br />
-    Check-in <? print $hotelInformationResponse['HotelDetails']['CheckInTime'] ?>
-    Check-out <? print $hotelInformationResponse['HotelDetails']['CheckOutTime'] ?><br />
+    Number of rooms: <? print $hotel['HotelDetails']['numberOfRooms'] ?><br />
+    Number of floors: <? print $hotel['HotelDetails']['numberOfFloors'] ?><br />
+    <?php if (isset($hotel['HotelDetails']['CheckInTime'])) { ?>Check-in <? print $hotel['HotelDetails']['CheckInTime']; } ?>
+    <?php if (isset($hotel['HotelDetails']['CheckOutTime'])) { ?>Check-out <? print $hotel['HotelDetails']['CheckOutTime']; } ?><br />
   </div>
 
   <div class="ean-hotel-images">
-    <? foreach ($hotelInformationReponse['HotelImages']['HotelImage'] as $image): ?>
+    <? foreach ($hotel['HotelImages']['HotelImage'] as $image): ?>
     <img
       src="<? print $image['url'] ?>"
+      <?php if (isset($image['caption'])): ?>
       alt="<? print $image['caption'] ?>"
       title="<? print $image['caption'] ?>"
+      <?php endif; ?>
     />
     <? endforeach ?>
   </div>
 
   <div class="ean-hotel-rooms">
-    <? foreach ($hotelInformationResponse['Roomtypes']['RoomType'] as $room): ?>
+    <? foreach ($hotel['RoomTypes']['RoomType'] as $room): ?>
       <? print $room['description']  ?><br />
       <p><? print $room['descriptionLong'] ?></p>
     <? endforeach ?>
