900bc32ad63c826e14deb8e720cc72e6261c6a31
Noe Snaterse <noe@noe-home.(none)>
2012-12-03 21:53:17 +0100

    And the search module with a template.

---
+++ ean-search-results.tpl.php
@@ -0,0 +1,6 @@
+<div class="hotel">
+  <img style="float: left; padding-right: 5px;" src="http://images.travelnow.com/<?php print $hotel['thumbNailUrl'] ?>" />
+  <?php print $hotel['address1'] ?><br />                                       
+  <?php print $hotel['postalCode'] ?> <?php print $hotel['city'] ?><br />       
+  <br style="clear: both" />
+</div>
