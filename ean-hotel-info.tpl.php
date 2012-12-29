3f59ef0080bbfb02a6d52aab8cd7a912683ae0df
Noë Snaterse <info@wiredpea.com>
2012-12-29 13:14:52 +0100

    Finished the field formatter for the hotels.

---
+++ ean-hotel-info.tpl.php
@@ -0,0 +1,25 @@
+<h2><?php print($hotel['HotelSummary']['name']) ?></h2>
+<?php print($hotel['HotelSummary']['hotelRating']) ?> stars
+<div class="address">
+<?php print($hotel['HotelSummary']['address1']) ?><br />
+<?php print($hotel['HotelSummary']['postalCode']) ?>
+<?php print($hotel['HotelSummary']['city']) ?>
+</div>
+<div class="checkin-checkout">
+Checkin: <?php print($hotel['HotelDetails']['checkInTime']) ?><br />
+Checkout: <?php print($hotel['HotelDetails']['checkOutTime']) ?><br />
+<small><?php print($hotel['HotelDetails']['checkInInstructions']) ?></small>
+</div>
+<div class="images">
+  <?php foreach ($hotel['HotelImages']['HotelImage'] as $image): ?>
+    <img src="<?php print($image['thumbnailUrl']) ?>" />
+  <?php endforeach ?>
+</div>
+<div class="description">
+<?php print(str_replace('&gt;', '>', str_replace('&lt;', '<', $hotel['HotelDetails']['propertyDescription']))) ?>
+</div>
+<div class="propertyAmenities">
+<?php foreach ($hotel['PropertyAmenities']['PropertyAmenity'] as $amenity):?>
+  <?php print($amenity['amenity']) ?>
+<?php endforeach ?>
+</div>
