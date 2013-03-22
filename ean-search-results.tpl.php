a84acc21d7c559db7ea0491d635c5ad465c12852
Noe Snaterse <noe@wiredpea.com>
2013-03-22 13:54:35 +0100

    Documented the search results template

--- ean-search-results.tpl.php
+++ ean-search-results.tpl.php
@@ -1,6 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for the EAN search module.
+ * It shows the user how they can implement their own templates.
+ */
+?>
 <div class="hotel">
   <img style="float: left; padding-right: 5px;" src="http://images.travelnow.com/<?php print $hotel['thumbNailUrl'] ?>" />
-  <?php print $hotel['address1'] ?><br />                                       
-  <?php print $hotel['postalCode'] ?> <?php print $hotel['city'] ?><br />       
+  <?php print $hotel['address1'] ?><br />
+  <?php print $hotel['postalCode'] ?> <?php print $hotel['city'] ?><br />
   <br style="clear: both" />
 </div>
